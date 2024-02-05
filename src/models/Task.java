d76eead1e9f99c949f558d30dddcc056c55d0c0b
masas <masas@DESKTOP-JE61OCU>
2024-02-05 20:06:27 +0900

    修正

--- src/models/Task.java
+++ src/models/Task.java
@@ -14,16 +14,19 @@ import javax.persistence.Table;
 @Entity
 @NamedQueries({
     @NamedQuery(
-        name = "getAllMessages",
-        query = "SELECT m FROM Message AS m ORDER BY m.id DESC"
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
     )
 })
 @Table(name = "tasks")
-public class Message {
+public class Task {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
